1902c892bfe4c7418e70b338d963c48ae5a1a088
Luca Marcelli <ionix@protonmail.com>
2017-06-13 11:03:59 +0200

    add variables and change from hex to dec
    format changes and adding vars

--- r8/src/screen.rs
+++ r8/src/screen.rs
@@ -2,6 +2,7 @@ pub struct Screen {
     width: u32,
     height: u32,
     size: u32,
+    screen: [[u32; 32]; 64],
 }
 
 impl Screen {
@@ -10,6 +11,7 @@ impl Screen {
             width: 64,
             height: 32,
             size: 64 * 32,
+            screen: [[0x000; 32]; 64], // todo: not sure if 2d is a good idea
         }
     }
 }
