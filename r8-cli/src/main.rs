2c57e6f70c715a1cbb70c590cc0dffead3f068b6
Luca Marcelli <ionix@protonmail.com>
2017-06-13 23:43:51 +0200

    impl reading
    implemented rom reading

--- r8-cli/src/main.rs
+++ r8-cli/src/main.rs
@@ -1,8 +1,19 @@
 extern crate r8;
 
 use r8::cpu;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("Hello, world!");
-    let r = cpu::CPU::new();
+    let rom_path = env::args().nth(1).unwrap();
+    let mut rom_file = File::open(rom_path).unwrap();
+    let mut buffer = Vec::new();
+    rom_file.read_to_end(&mut buffer);
+    let mut rom_buffer = Vec::<char>::new();
+    for byte in buffer {
+        rom_buffer.push(byte as char);
+    }
+    let r = cpu::CPU::new(rom_buffer);
 }
