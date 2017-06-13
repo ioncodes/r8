dc2d9b8972a30bc59e4a2109665571a9ac376270
Luca Marcelli <ionix@protonmail.com>
2017-06-13 10:45:36 +0200

    unused import warning
    removed the cpu import unused warning

--- r8-cli/src/main.rs
+++ r8-cli/src/main.rs
@@ -4,5 +4,5 @@ use r8::cpu;
 
 fn main() {
     println!("Hello, world!");
-    let r = r8::cpu::CPU::new();
+    let r = cpu::CPU::new();
 }
--- r8/src/r8.rs
+++ r8/src/r8.rs
@@ -6,6 +6,7 @@ pub const ENTRY_POINT: u32 = 0x200;
 pub const FONT_SIZE: u32 = 0x50;
 pub const SCREEN: Screen = Screen::new();
 
+
 pub struct R8 {
     pub program_counter: u32,
     pub stack_pointer: u32,
