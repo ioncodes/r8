425a080c2492239eb4f981f3c2c548c129f23dfb
Luca Marcelli <ionix@protonmail.com>
2017-06-14 11:43:27 +0200

    debug and impl rom loading in cli

--- r8-cli/src/main.rs
+++ r8-cli/src/main.rs
@@ -1,13 +1,14 @@
 extern crate r8;
 
-use r8::cpu;
+use r8::cpu::CPU;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 
 fn main() {
     println!("Hello, world!");
-    let rom_path = env::args().nth(1).unwrap();
+    //let mut rom_path = env::args().nth(1).unwrap();
+    let mut rom_path = "../roms/BLITZ".to_owned();
     let mut rom_file = File::open(rom_path).unwrap();
     let mut buffer = Vec::new();
     rom_file.read_to_end(&mut buffer);
@@ -15,5 +16,6 @@ fn main() {
     for byte in buffer {
         rom_buffer.push(byte as char);
     }
-    let r = cpu::CPU::new(rom_buffer);
+    let cpu = CPU::new(rom_buffer);
+    cpu.load_rom(); // load the rom
 }
